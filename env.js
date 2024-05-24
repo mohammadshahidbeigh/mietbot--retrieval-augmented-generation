beda739779f6772a2596475e01ee3eae9fc9048b
Mohammad Shahid Beigh <85876937+mohammadshahidbeigh@users.noreply.github.com>
2024-05-24 22:40:33 +0530

    Update env.js

--- env.js
+++ env.js
@@ -1,11 +1,11 @@
 // import { ChatOpenAI } from "@langchain/openai";
 
 // const chatModel = new ChatOpenAI({
-//   openAIApiKey: "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",
+//   openAIApiKey: "sk-",
 // });
 
 export const process = {
   env: {
-    OPENAI_API_KEY: "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", // Your OpenAI API Keyy
+    OPENAI_API_KEY: "sk-", // Your OpenAI API Keyy
   },
 };
